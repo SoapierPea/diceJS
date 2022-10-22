a8539eda23eac5dd1f9ba9f9460d3d09713bda45
kevin rodriguez <kevinr4839@gmail.com>
2022-10-21 20:36:20 -0400

    first commit

---
+++ dice.js
@@ -0,0 +1,22 @@
+let die1 =[]
+let die2 =[]
+let die3 =[]
+let die4 =[]
+let die5 =[]
+let totScore = 0
+let roundScore = 0
+class Die{
+    function dieRoll(){
+    let die = []
+    let randNum = Math.floor(Math.random()*6)+1
+    die.push(randNum)
+
+}
+
+    function diePoints(die){
+        if (die[i]>0 && die[i]<7){
+
+        }
+    }
+
+}
